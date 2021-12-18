6d2598dcc267fc060762277a884efb427e501c16
Andrey Tikhonov <andrey.tikhonov@quantori.com>
2021-12-18 16:15:48 +0300

    more api updates

--- clepsydra/api/rules.py
+++ clepsydra/api/rules.py
@@ -1,13 +1,29 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from typing import Union
+from typing import Protocol, Optional
+
+
+class Rule(Protocol):
+    def get_next(self, after: datetime) -> Optional[datetime]:
+        raise NotImplementedError
 
 
 @dataclass
-class IntervalRule:
+class IntervalRule(Rule):
     start: datetime
     period: timedelta
 
+    def get_next(self, after: datetime) -> datetime:
+        delta = after - self.start
+        intervals = delta.total_seconds() // self.period.total_seconds() + 1
+        return self.start + intervals * self.period
 
-OneTimeRule = datetime
-Rule = Union[OneTimeRule, IntervalRule]
+
+@dataclass
+class SingleRun(Rule):
+    when: datetime
+
+    def get_next(self, after: datetime):
+        if after > self.when:
+            return None
+        return self.when
--- clepsydra/api/storage.py
+++ clepsydra/api/storage.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Protocol, Tuple, Any, Optional
+from typing import Protocol, Tuple, Any, Optional, List
 
 from .rules import Rule
 
@@ -13,16 +13,31 @@ class JobInfo:
     kwargs: Tuple[Any]
     rule: Rule
     next_start: datetime
+    created_at: datetime
 
 
 class Storage(Protocol):
-    def get_next_job(self) -> JobInfo:
+    def get_next_job(
+            self,
+            after: Optional[datetime] = None,
+            before: Optional[datetime] = None,
+    ) -> JobInfo:
+        pass
+
+    def get_jobs(
+            self,
+            next_after: Optional[datetime] = None,
+            next_before: Optional[datetime] = None,
+    ) -> List[JobInfo]:
         pass
 
     def schedule_next(self, job_id: int, next_start: datetime):
         pass
 
-    def mark_completed(self, job_id: int):
+    def mark_completed(self, job_id: int):  # TODO what if not single run
+        pass
+
+    def mark_started(self, job_id: int):
         pass
 
     def save_task(self, job: JobInfo):
